ac86e546dc0065d65f4c1fb16052e5cec03ddc8b
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-09-02 14:09:21 -0700

    Initial commit, remove public index file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreEngine::Application.config.secret_token = '080098beda82b4608a7807d9186660b2523f80ce05fb91fc35b50ffebc555951718469c90ee07c4a7dc7053ab4bb6c68883a8338595f7f3adee49d275aa0e6fa'
